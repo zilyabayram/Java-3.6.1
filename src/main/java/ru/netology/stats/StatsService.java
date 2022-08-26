702f1dcf7a518922524473c7c054997b852a13b3
Zilia Bairamova <ziliabairamova@gmail.com>
2022-08-26 11:25:56 +0300

    code fixed

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -3,22 +3,22 @@ package ru.netology.stats;
 public class StatsService {
 
     public int getSumOfAllSales(int[] sales) {
-        int SumOfSales = 0;
+        int sumOfSales = 0;
         for (int i = 0; i < sales.length; i++) {
-            SumOfSales = SumOfSales + sales[i];
+            sumOfSales = sumOfSales + sales[i];
         }
-        return SumOfSales;
+        return sumOfSales;
     }
 
     public int averageOfAllSales(int[] sales) {
         return getSumOfAllSales(sales) / sales.length;
     }
 
-    public int maxSalesMonth (int[] sales) {
+    public int maxSalesMonth(int[] sales) {
         int maxMonth = 0;
         int month = 0;
         for (int sale : sales) {
-            if (sale >= sales [maxMonth]) {
+            if (sale >= sales[maxMonth]) {
                 maxMonth = month;
             }
             month = month + 1;
@@ -26,11 +26,11 @@ public class StatsService {
         return maxMonth + 1;
     }
 
-    public int minSalesMonth (int[] sales) {
+    public int minSalesMonth(int[] sales) {
         int minMonth = 0;
         int month = 0;
         for (int sale : sales) {
-            if (sale <= sales [minMonth]) {
+            if (sale <= sales[minMonth]) {
                 minMonth = month;
             }
             month = month + 1;
@@ -38,20 +38,22 @@ public class StatsService {
         return minMonth + 1;
     }
 
-    public int SumOfMonthsLessThanAverage (int [] sales) {
-        int averageMonth =0;
+    public int SumOfMonthsLessThanAverage(int[] sales) {
+        int averageMonth = 0;
+        int averageOfAllSales = getSumOfAllSales(sales) / sales.length;
         for (int sale : sales) {
-            if (sale < averageOfAllSales(sales)) {
+            if (sale < averageOfAllSales) {
                 averageMonth = averageMonth + 1;
             }
         }
         return averageMonth;
     }
 
-    public int SumOfMonthsMoreThanAverage (int [] sales) {
-        int averageMonth =0;
+    public int SumOfMonthsMoreThanAverage(int[] sales) {
+        int averageMonth = 0;
+        int averageOfAllSales = getSumOfAllSales(sales) / sales.length;
         for (int sale : sales) {
-            if (sale > averageOfAllSales(sales)) {
+            if (sale > averageOfAllSales) {
                 averageMonth = averageMonth + 1;
             }
         }
